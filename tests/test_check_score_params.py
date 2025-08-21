26985e304a2dfaa158144bbe80b92d0a2dd5dc9f
Mikhail Martin <JohnDoe110@yandex.ru>
2025-08-21 17:37:50 +0300

    refactoring

--- tests/test_check_score_params.py
+++ tests/test_check_score_params.py
@@ -1,7 +1,7 @@
 from contextlib import nullcontext as does_not_raise
 import os
 import sys
-sys.path.append(sys.path[0] + '/../')
+sys.path.append(sys.path[0] + "/../")
 
 import pandas as pd
 import pytest
@@ -10,39 +10,39 @@ from pytest import param, raises
 from smarttree import SmartTreeClassifier
 
 
-data = pd.read_csv(os.path.join('tests', 'test_dataset.csv'), index_col=0)
-X = data[['2. Возраст', '3. Семейное положение', '5. В какой семье Вы выросли?']]
-y = data['Метка']
+data = pd.read_csv(os.path.join("tests", "test_dataset.csv"), index_col=0)
+X = data[["2. Возраст", "3. Семейное положение", "5. В какой семье Вы выросли?"]]
+y = data["Метка"]
 
 
 @pytest.mark.parametrize(
-    ('X', 'y', 'expected'),
+    ("X", "y", "expected"),
     [
         param(X, y, does_not_raise()),
         param(
-            'X', y,
-            raises(ValueError, match='X must be a pandas.DataFrame.'),
+            "X", y,
+            raises(ValueError, match="X must be a pandas.DataFrame."),
         ),
         param(
-            X, 'y',
+            X, "y",
             raises(
                 ValueError,
-                match='y must be a pandas.Series.',
+                match="y must be a pandas.Series.",
             ),
         ),
         param(
             X, y[:-1],
-            raises(ValueError, match='X and y must be the equal length.'),
+            raises(ValueError, match="X and y must be the equal length."),
         ),
         param(
-            X.rename(columns={'2. Возраст': '2. Age'}), y,
+            X.rename(columns={"2. Возраст": "2. Age"}), y,
             raises(
                 ValueError,
                 match=(
-                    'Feature names unseen at fit time:\n'
-                    '- 2. Age\n'
-                    'Feature names seen at fit time, yet now missing:\n'
-                    '- 2. Возраст\n'
+                    "Feature names unseen at fit time:\n"
+                    "- 2. Age\n"
+                    "Feature names seen at fit time, yet now missing:\n"
+                    "- 2. Возраст\n"
                 ),
             ),
         ),
@@ -50,21 +50,21 @@ y = data['Метка']
 )
 def test_check_score_params(X, y, expected):
     with expected:
-        data = pd.read_csv(os.path.join('tests', 'test_dataset.csv'), index_col=0)
-        X_fit = data[['2. Возраст', '3. Семейное положение', '5. В какой семье Вы выросли?']]
-        y_fit = data['Метка']
+        data = pd.read_csv(os.path.join("tests", "test_dataset.csv"), index_col=0)
+        X_fit = data[["2. Возраст", "3. Семейное положение", "5. В какой семье Вы выросли?"]]
+        y_fit = data["Метка"]
 
         msdt = SmartTreeClassifier(
             max_depth=1,
-            numerical_feature_names=['2. Возраст'],
-            categorical_feature_names=['3. Семейное положение'],
+            numerical_feature_names=["2. Возраст"],
+            categorical_feature_names=["3. Семейное положение"],
             rank_feature_names={
-                '5. В какой семье Вы выросли?': [
-                    'полная семья, кровные родители',
-                    'мачеха/отчим',
-                    'мать/отец одиночка',
-                    'с бабушкой и дедушкой',
-                    'в детском доме',
+                "5. В какой семье Вы выросли?": [
+                    "полная семья, кровные родители",
+                    "мачеха/отчим",
+                    "мать/отец одиночка",
+                    "с бабушкой и дедушкой",
+                    "в детском доме",
                 ],
             },
         )
