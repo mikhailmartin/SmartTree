81ef00f6563bbdf3a38175ff85292a4e614b4cb6
Mikhail Martin <JohnDoe110@yandex.ru>
2025-08-21 17:33:41 +0300

    refactoring

--- tests/test_repr_tree.py
+++ tests/test_repr_tree.py
@@ -5,10 +5,10 @@ from smarttree import SmartTreeClassifier
 
 
 @pytest.mark.parametrize(
-    ('criterion', 'expected'),
+    ("criterion", "expected"),
     [
-        param('gini', f'{SmartTreeClassifier.__name__}()'),
-        param('entropy', f"{SmartTreeClassifier.__name__}(criterion='entropy')"),
+        param("gini", f"{SmartTreeClassifier.__name__}()"),
+        param("entropy", f"{SmartTreeClassifier.__name__}(criterion='entropy')"),
     ],
 )
 def test_repr_tree__criterion(criterion, expected):
@@ -17,10 +17,10 @@ def test_repr_tree__criterion(criterion, expected):
 
 
 @pytest.mark.parametrize(
-    ('max_depth', 'expected'),
+    ("max_depth", "expected"),
     [
-        param(None, f'{SmartTreeClassifier.__name__}()'),
-        param(1, f'{SmartTreeClassifier.__name__}(max_depth=1)'),
+        param(None, f"{SmartTreeClassifier.__name__}()"),
+        param(1, f"{SmartTreeClassifier.__name__}(max_depth=1)"),
     ],
 )
 def test_repr_tree__max_depth(max_depth, expected):
@@ -29,11 +29,11 @@ def test_repr_tree__max_depth(max_depth, expected):
 
 
 @pytest.mark.parametrize(
-    ('min_samples_split', 'expected'),
+    ("min_samples_split", "expected"),
     [
-        param(2, f'{SmartTreeClassifier.__name__}()'),
-        param(3, f'{SmartTreeClassifier.__name__}(min_samples_split=3)'),
-        param(.5, f'{SmartTreeClassifier.__name__}(min_samples_split=0.5)'),
+        param(2, f"{SmartTreeClassifier.__name__}()"),
+        param(3, f"{SmartTreeClassifier.__name__}(min_samples_split=3)"),
+        param(.5, f"{SmartTreeClassifier.__name__}(min_samples_split=0.5)"),
     ],
 )
 def test_repr_tree__min_samples_split(min_samples_split, expected):
@@ -42,11 +42,11 @@ def test_repr_tree__min_samples_split(min_samples_split, expected):
 
 
 @pytest.mark.parametrize(
-    ('min_samples_leaf', 'expected'),
+    ("min_samples_leaf", "expected"),
     [
-        param(1, f'{SmartTreeClassifier.__name__}()'),
-        param(2, f'{SmartTreeClassifier.__name__}(min_samples_leaf=2)'),
-        param(.5, f'{SmartTreeClassifier.__name__}(min_samples_leaf=0.5)'),
+        param(1, f"{SmartTreeClassifier.__name__}()"),
+        param(2, f"{SmartTreeClassifier.__name__}(min_samples_leaf=2)"),
+        param(.5, f"{SmartTreeClassifier.__name__}(min_samples_leaf=0.5)"),
     ],
 )
 def test_repr_tree__min_samples_leaf(min_samples_leaf, expected):
@@ -55,10 +55,10 @@ def test_repr_tree__min_samples_leaf(min_samples_leaf, expected):
 
 
 @pytest.mark.parametrize(
-    ('max_leaf_nodes', 'expected'),
+    ("max_leaf_nodes", "expected"),
     [
-        param(float('+inf'), f'{SmartTreeClassifier.__name__}()'),
-        param(2, f'{SmartTreeClassifier.__name__}(max_leaf_nodes=2)'),
+        param(float("+inf"), f"{SmartTreeClassifier.__name__}()"),
+        param(2, f"{SmartTreeClassifier.__name__}(max_leaf_nodes=2)"),
     ],
 )
 def test_repr_tree__max_leaf_nodes(max_leaf_nodes, expected):
@@ -67,10 +67,10 @@ def test_repr_tree__max_leaf_nodes(max_leaf_nodes, expected):
 
 
 @pytest.mark.parametrize(
-    ('min_impurity_decrease', 'expected'),
+    ("min_impurity_decrease", "expected"),
     [
-        param(.0, f'{SmartTreeClassifier.__name__}()'),
-        param(.5, f'{SmartTreeClassifier.__name__}(min_impurity_decrease=0.5)'),
+        param(.0, f"{SmartTreeClassifier.__name__}()"),
+        param(.5, f"{SmartTreeClassifier.__name__}(min_impurity_decrease=0.5)"),
     ],
 )
 def test_repr_tree__min_impurity_decrease(min_impurity_decrease, expected):
@@ -79,10 +79,10 @@ def test_repr_tree__min_impurity_decrease(min_impurity_decrease, expected):
 
 
 @pytest.mark.parametrize(
-    ('max_childs', 'expected'),
+    ("max_childs", "expected"),
     [
-        param(float('+inf'), f'{SmartTreeClassifier.__name__}()'),
-        param(2, f'{SmartTreeClassifier.__name__}(max_childs=2)'),
+        param(float("+inf"), f"{SmartTreeClassifier.__name__}()"),
+        param(2, f"{SmartTreeClassifier.__name__}(max_childs=2)"),
     ],
 )
 def test_repr_tree__max_childs(max_childs, expected):
@@ -91,11 +91,11 @@ def test_repr_tree__max_childs(max_childs, expected):
 
 
 @pytest.mark.parametrize(
-    ('numerical_feature_names', 'expected'),
+    ("numerical_feature_names", "expected"),
     [
-        param(None, f'{SmartTreeClassifier.__name__}()'),
-        param('feature', f"{SmartTreeClassifier.__name__}(numerical_feature_names=['feature'])"),
-        param(['feature'], f"{SmartTreeClassifier.__name__}(numerical_feature_names=['feature'])"),
+        param(None, f"{SmartTreeClassifier.__name__}()"),
+        param("feature", f"{SmartTreeClassifier.__name__}(numerical_feature_names=['feature'])"),
+        param(["feature"], f"{SmartTreeClassifier.__name__}(numerical_feature_names=['feature'])"),
     ],
 )
 def test_repr_tree__numerical_feature_names(numerical_feature_names, expected):
@@ -105,11 +105,11 @@ def test_repr_tree__numerical_feature_names(numerical_feature_names, expected):
 
 
 @pytest.mark.parametrize(
-    ('categorical_feature_names', 'expected'),
+    ("categorical_feature_names", "expected"),
     [
-        param(None, f'{SmartTreeClassifier.__name__}()'),
-        param('feature', f"{SmartTreeClassifier.__name__}(categorical_feature_names=['feature'])"),
-        param(['feature'], f"{SmartTreeClassifier.__name__}(categorical_feature_names=['feature'])"),
+        param(None, f"{SmartTreeClassifier.__name__}()"),
+        param("feature", f"{SmartTreeClassifier.__name__}(categorical_feature_names=['feature'])"),
+        param(["feature"], f"{SmartTreeClassifier.__name__}(categorical_feature_names=['feature'])"),
     ],
 )
 def test_repr_tree__categorical_feature_names(categorical_feature_names, expected):
@@ -119,10 +119,10 @@ def test_repr_tree__categorical_feature_names(categorical_feature_names, expecte
 
 
 @pytest.mark.parametrize(
-    ('rank_feature_names', 'expected'),
+    ("rank_feature_names", "expected"),
     [
-        param(None, f'{SmartTreeClassifier.__name__}()'),
-        param({'f': ['v1', 'v2']}, f"{SmartTreeClassifier.__name__}(rank_feature_names={{'f': ['v1', 'v2']}})"),
+        param(None, f"{SmartTreeClassifier.__name__}()"),
+        param({"f": ["v1", "v2"]}, f"{SmartTreeClassifier.__name__}(rank_feature_names={{'f': ['v1', 'v2']}})"),
     ],
 )
 def test_repr_tree__rank_feature_names(rank_feature_names, expected):
@@ -131,11 +131,11 @@ def test_repr_tree__rank_feature_names(rank_feature_names, expected):
 
 
 @pytest.mark.parametrize(
-    ('hierarchy', 'expected'),
+    ("hierarchy", "expected"),
     [
-        param(None, f'{SmartTreeClassifier.__name__}()'),
-        param({'f': 'f1'}, f"{SmartTreeClassifier.__name__}(hierarchy={{'f': 'f1'}})"),
-        param({'f': ['f1', 'f2']}, f"{SmartTreeClassifier.__name__}(hierarchy={{'f': ['f1', 'f2']}})"),
+        param(None, f"{SmartTreeClassifier.__name__}()"),
+        param({"f": "f1"}, f"{SmartTreeClassifier.__name__}(hierarchy={{'f': 'f1'}})"),
+        param({"f": ["f1", "f2"]}, f"{SmartTreeClassifier.__name__}(hierarchy={{'f': ['f1', 'f2']}})"),
     ],
 )
 def test_repr_tree__hierarchy(hierarchy, expected):
@@ -144,11 +144,11 @@ def test_repr_tree__hierarchy(hierarchy, expected):
 
 
 @pytest.mark.parametrize(
-    ('numerical_nan_mode', 'expected'),
+    ("numerical_nan_mode", "expected"),
     [
-        param('min', f'{SmartTreeClassifier.__name__}()'),
-        param('max', f"{SmartTreeClassifier.__name__}(numerical_nan_mode='max')"),
-        param('include', f"{SmartTreeClassifier.__name__}(numerical_nan_mode='include')"),
+        param("min", f"{SmartTreeClassifier.__name__}()"),
+        param("max", f"{SmartTreeClassifier.__name__}(numerical_nan_mode='max')"),
+        param("include", f"{SmartTreeClassifier.__name__}(numerical_nan_mode='include')"),
     ],
 )
 def test_repr_tree__numerical_nan_mode(numerical_nan_mode, expected):
@@ -157,10 +157,10 @@ def test_repr_tree__numerical_nan_mode(numerical_nan_mode, expected):
 
 
 @pytest.mark.parametrize(
-    ('categorical_nan_mode', 'expected'),
+    ("categorical_nan_mode", "expected"),
     [
-        param('include', f'{SmartTreeClassifier.__name__}()'),
-        param('as_category', f"{SmartTreeClassifier.__name__}(categorical_nan_mode='as_category')"),
+        param("include", f"{SmartTreeClassifier.__name__}()"),
+        param("as_category", f"{SmartTreeClassifier.__name__}(categorical_nan_mode='as_category')"),
     ],
 )
 def test_repr_tree__categorical_nan_mode(categorical_nan_mode, expected):
@@ -169,10 +169,10 @@ def test_repr_tree__categorical_nan_mode(categorical_nan_mode, expected):
 
 
 @pytest.mark.parametrize(
-    ('categorical_nan_filler', 'expected'),
+    ("categorical_nan_filler", "expected"),
     [
-        param('missing_value', f'{SmartTreeClassifier.__name__}()'),
-        param('NULL', f"{SmartTreeClassifier.__name__}(categorical_nan_filler='NULL')"),
+        param("missing_value", f"{SmartTreeClassifier.__name__}()"),
+        param("NULL", f"{SmartTreeClassifier.__name__}(categorical_nan_filler='NULL')"),
     ],
 )
 def test_repr_tree__categorical_nan_filler(categorical_nan_filler, expected):
