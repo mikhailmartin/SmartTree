fdf3663a50204acfc23b0bbd251a1ce86c0f5f60
Mikhail Martin <JohnDoe110@yandex.ru>
2025-08-26 17:05:24 +0300

    NEW: NumericalColumnSplitter

--- smarttree/_column_splitter.py
+++ smarttree/_column_splitter.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
 import math
 
+import numpy as np
 import pandas as pd
 
-from smarttree._utils import cat_partitions, get_thresholds, rank_partitions
+# from smarttree._utils import cat_partitions, rank_partitions
 
 
 class BaseColumnSplitter:
@@ -139,3 +140,107 @@ class BaseColumnSplitter:
                 entropy -= p_i * math.log2(p_i)
 
         return entropy
+
+
+class NumericalColumnSplitter(BaseColumnSplitter):
+
+    def __init__(
+        self,
+        X: pd.DataFrame,
+        y: pd.Series,
+        criterion,
+        max_depth,
+        min_samples_split,
+        min_samples_leaf,
+        numerical_nan_mode,  # TODO: annotation
+    ) -> None:
+        super().__init__(
+            X=X,
+            y=y,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+        )
+        self.numerical_nan_mode = numerical_nan_mode
+
+    def split(
+        self,
+        parent_mask: pd.Series,
+        split_feature_name: str,
+    ) -> tuple[float, list[list[str]] | None, list[pd.Series] | None]:
+        """
+        Finds the best tree node split by set numerical feature, if it exists.
+
+        Parameters:
+            parent_mask: boolean mask of the tree node.
+            split_feature_name: The name of the set numerical feature by which to find
+              the best split.
+
+        Returns:
+            Tuple `(inf_gain, feature_values, child_masks)`.
+              inf_gain: information gain of the split.
+              feature_values: feature values corresponding to child nodes.
+              child_masks: boolean masks of child nodes.
+        """
+        use_including_na = (
+            self.numerical_nan_mode == "include"
+            # and there are samples with missing values
+            and (parent_mask & self.X[split_feature_name].isna()).sum()
+        )
+
+        if use_including_na:
+            mask_notna = parent_mask & self.X[split_feature_name].notna()
+            # if split by feature value is not possible
+            if mask_notna.sum() <= 1:
+                return float("-inf"), None, None
+            mask_na = parent_mask & self.X[split_feature_name].isna()
+
+            points = self.X.loc[mask_notna, split_feature_name].to_numpy()
+        else:
+            points = self.X.loc[parent_mask, split_feature_name].to_numpy()
+
+        thresholds = self.get_thresholds(points)
+
+        best_inf_gain = float("-inf")
+        best_feature_values = None
+        best_child_masks = None
+        for threshold in thresholds:
+            mask_less = parent_mask & (self.X[split_feature_name] <= threshold)
+            mask_more = parent_mask & (self.X[split_feature_name] > threshold)
+
+            if use_including_na:
+                mask_less = mask_less | mask_na
+                mask_more = mask_more | mask_na
+
+            if (
+                mask_less.sum() < self.min_samples_leaf
+                or mask_more.sum() < self.min_samples_leaf
+            ):
+                continue
+
+            child_masks = [mask_less, mask_more]
+
+            inf_gain = self.information_gain(
+                parent_mask, child_masks, nan_mode=self.numerical_nan_mode
+            )
+
+            if best_inf_gain < inf_gain:
+                best_inf_gain = inf_gain
+                less_values = [f"<= {threshold}"]
+                more_values = [f"> {threshold}"]
+                best_feature_values = [less_values, more_values]
+                best_child_masks = child_masks
+
+        return best_inf_gain, best_feature_values, best_child_masks
+
+    def get_thresholds(self, array: np.ndarray) -> np.ndarray:
+        array.sort()
+        array = np.unique(array)
+        thresholds = np.array([]) if len(array) == 1 else self.moving_average(array, 2)
+
+        return thresholds
+
+    @staticmethod
+    def moving_average(array: np.ndarray, window: int) -> np.ndarray:
+        return np.convolve(array, np.ones(window), mode="valid") / window
---
+++ tests/column_splitter/test__numerical_column_splitter.py
@@ -0,0 +1,25 @@
+import pytest
+
+from smarttree._column_splitter import NumericalColumnSplitter
+
+
+@pytest.fixture(scope="module")
+def numerical_column_splitter(X, y):
+    return NumericalColumnSplitter(
+        X=X,
+        y=y,
+        criterion="gini",
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        numerical_nan_mode="min",
+    )
+
+
+def test__split(numerical_column_splitter, y):
+
+    parent_mask = y.apply(lambda x: True)
+    split_feature_name = "2. Возраст"
+    inf_gain, feature_values, child_masks = numerical_column_splitter.split(
+        parent_mask, split_feature_name
+    )
