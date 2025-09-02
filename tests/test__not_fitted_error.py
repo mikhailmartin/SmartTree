0ce6cbf0303c6dbb5b4299023dea4928dc3778a5
Mikhail Martin <JohnDoe110@yandex.ru>
2025-09-02 16:52:49 +0300

    refactoring: test__not_fitted

--- tests/test__not_fitted_error.py
+++ tests/test__not_fitted_error.py
@@ -10,39 +10,30 @@ def not_fitted_tree() -> BaseSmartDecisionTree:
     return SmartDecisionTreeClassifier()
 
 
-def test__tree(not_fitted_tree):
+@pytest.mark.parametrize(
+    "method_call",
+    [
+        lambda tree, X, y: tree.predict(X),
+        lambda tree, X, y: tree.predict_proba(X),
+        lambda tree, X, y: tree.score(X, y),
+        lambda tree, X, y: tree.render(),
+    ],
+    ids=["predict", "predict_proba", "score", "render"],
+)
+def test__not_fitted__method(not_fitted_tree, X, y, method_call):
     with raises(NotFittedError):
-        _ = not_fitted_tree.tree
+        method_call(not_fitted_tree, X, y)
 
 
-def test__feature_names(not_fitted_tree):
+@pytest.mark.parametrize(
+    "property_name",
+    ["tree", "feature_names", "feature_importances_"],
+    ids=lambda param: str(param),
+)
+def test__not_fitted__property(not_fitted_tree, property_name):
     with raises(NotFittedError):
-        _ = not_fitted_tree.feature_names
-
-
-def test__feature_importances(not_fitted_tree):
-    with raises(NotFittedError):
-        _ = not_fitted_tree.feature_importances_
-
-
-def test__predict(not_fitted_tree):
-    with raises(NotFittedError):
-        _ = not_fitted_tree.predict(None)
-
-
-def test__predict_proba(not_fitted_tree, X):
-    with raises(NotFittedError):
-        _ = not_fitted_tree.predict_proba(X)
-
-
-def test__score(not_fitted_tree, X, y):
-    with raises(NotFittedError):
-        _ = not_fitted_tree.score(X, y)
-
-
-def test__render(not_fitted_tree):
-    with raises(NotFittedError):
-        _ = not_fitted_tree.render()
+        property_ = getattr(not_fitted_tree, property_name)
+        _ = property_
 
 
 @pytest.fixture(scope="module")
