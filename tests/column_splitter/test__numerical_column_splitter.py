3a3b54c5e98eb7b9789dc77c1a57a96384aba33d
Mikhail Martin <JohnDoe110@yandex.ru>
2025-09-07 00:46:16 +0300

    refactoring

--- tests/column_splitter/test__numerical_column_splitter.py
+++ tests/column_splitter/test__numerical_column_splitter.py
@@ -1,6 +1,7 @@
 import pytest
 
 from smarttree._column_splitter import NumericalColumnSplitter
+from smarttree._constants import NumericalNanModeOption
 from smarttree._dataset import Dataset
 
 
@@ -14,8 +15,21 @@ def numerical_column_splitter(X, y) -> NumericalColumnSplitter:
         numerical_nan_mode="min",
     )
 
+@pytest.mark.parametrize(
+    "numerical_nan_mode",
+    ["min", "max", "include"],
+    ids=lambda param: str(param),
+)
+def test__split(X, y, numerical_nan_mode, root_node):
 
-def test__split(numerical_column_splitter, root_node):
+    numerical_nan_mode: NumericalNanModeOption
+    numerical_column_splitter = NumericalColumnSplitter(
+        dataset=Dataset(X, y),
+        criterion="gini",
+        min_samples_split=2,
+        min_samples_leaf=1,
+        numerical_nan_mode=numerical_nan_mode,
+    )
 
-    split_feature_name = "2. Возраст"
-    split_result = numerical_column_splitter.split(root_node, split_feature_name)
+    split_feature_name_with_nan = "2. Возраст"
+    split_result = numerical_column_splitter.split(root_node, split_feature_name_with_nan)
