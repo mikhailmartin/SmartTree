c746ecab218179e73fcf010c09c76a519fa7c7ca
Mikhail Martin <JohnDoe110@yandex.ru>
2025-09-25 19:26:51 +0300

    remove dataclass from dataset

--- smarttree/_dataset.py
+++ smarttree/_dataset.py
@@ -1,23 +1,17 @@
-from dataclasses import dataclass, field
-
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
 
 
-@dataclass
 class Dataset:
 
-    X: pd.DataFrame
-    y: pd.Series
-    class_names: NDArray = field(init=False)
-    has_na: dict[str, bool] = field(init=False)
-    mask_na: dict[str, pd.Series] = field(init=False)
+    def __init__(self, X: pd.DataFrame, y: pd.Series) -> None:
+        self.X = X
+        self.y = y
 
-    def __post_init__(self) -> None:
-        self.class_names = np.sort(self.y.unique())
-        self.has_na = dict()
-        self.mask_na = dict()
+        self.class_names: NDArray = np.sort(self.y.unique())
+        self.has_na: dict[str, bool] = dict()
+        self.mask_na: dict[str, pd.Series] = dict()
         for column in self.X.columns:
             mask_na = self.X[column].isna()
             has_na = mask_na.any()
