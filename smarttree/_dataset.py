a860feea7058f93b11d40db10f4a6ee29f35e7fe
Mikhail Martin <JohnDoe110@yandex.ru>
2025-09-29 19:01:49 +0300

    mask_na: ndarray

--- smarttree/_dataset.py
+++ smarttree/_dataset.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
 
 
 class Dataset:
@@ -9,13 +10,13 @@ class Dataset:
         self.classes = np.sort(y.unique())
         self.y = np.searchsorted(self.classes, y.to_numpy()).astype(np.int64)
         self.has_na: dict[str, bool] = dict()
-        self.mask_na: dict[str, pd.Series] = dict()
+        self.mask_na: dict[str, NDArray] = dict()
         for column in self.X.columns:
             mask_na = self.X[column].isna()
             has_na = mask_na.any()
             self.has_na[column] = has_na
             if has_na:
-                self.mask_na[column] = mask_na
+                self.mask_na[column] = mask_na.to_numpy()
 
     @property
     def size(self) -> int:
